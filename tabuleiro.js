91e9a7d55e03ca3f8fd90f1be3c5d4683a9f7fab
l0rrainy <142435583+l0rrainy@users.noreply.github.com>
2024-05-09 12:32:47 -0300

    Add files via upload

---
+++ tabuleiro.js
@@ -0,0 +1,114 @@
+class Tabuleiro {
+    constructor (cor, x, y, linha, coluna, altura, largura) {
+        this.cor = cor;
+        this.x = x;
+        this.y = y;
+        this.linha = linha;
+        this.coluna = coluna;
+        this.altura = altura;
+        this.largura = largura;
+    }
+}
+
+const cor1 = "#fff";
+const cor2 = "#000";
+const medida = "px";
+const larguraPadrao = 30;
+const alturaPadrao = 30;
+
+const celula1 = new Tabuleiro(cor1, "0", "0", "8", "a", alturaPadrao, larguraPadrao);
+const celula2 = new Tabuleiro(cor2, "30", "0", "8", "b", alturaPadrao, larguraPadrao);
+const celula3 = new Tabuleiro(cor1, "60", "0", "8", "c", alturaPadrao, larguraPadrao);
+const celula4 = new Tabuleiro(cor2, "90", "0", "8", "d", alturaPadrao, larguraPadrao);
+const celula5 = new Tabuleiro(cor1, "120", "0", "8", "e", alturaPadrao, larguraPadrao);
+const celula6 = new Tabuleiro(cor2, "150", "0", "8", "f", alturaPadrao, larguraPadrao);
+const celula7 = new Tabuleiro(cor1, "180", "0", "8", "g", alturaPadrao, larguraPadrao);
+const celula8 = new Tabuleiro(cor2, "210", "0", "8", "h", alturaPadrao, larguraPadrao);
+
+
+
+
+const celula9 = new Tabuleiro(cor2, "0", "30", "7", "a", alturaPadrao, larguraPadrao);
+const celula10 = new Tabuleiro(cor1, "30", "30", "7", "b", alturaPadrao, larguraPadrao);
+const celula11 = new Tabuleiro(cor2, "60", "30", "7", "c", alturaPadrao, larguraPadrao);
+const celula12 = new Tabuleiro(cor1, "90", "30", "7", "d", alturaPadrao, larguraPadrao);
+const celula13 = new Tabuleiro(cor2, "120", "30", "7", "e", alturaPadrao, larguraPadrao);
+const celula14 = new Tabuleiro(cor1, "150", "30", "7", "f", alturaPadrao, larguraPadrao);
+const celula15 = new Tabuleiro(cor2, "180", "30", "7", "g", alturaPadrao, larguraPadrao);
+const celula16 = new Tabuleiro(cor1, "210", "30", "7", "h", alturaPadrao, larguraPadrao);
+
+
+
+
+ 
+const celula17 = new Tabuleiro(cor1, "0", "60", "6", "a", alturaPadrao, larguraPadrao);
+const celula18 = new Tabuleiro(cor2, "30", "60", "6", "b", alturaPadrao, larguraPadrao);
+const celula19 = new Tabuleiro(cor1, "60", "60", "6", "c", alturaPadrao, larguraPadrao);
+const celula20 = new Tabuleiro(cor2, "90", "60", "6", "d", alturaPadrao, larguraPadrao);
+const celula21 = new Tabuleiro(cor1, "120", "60", "6", "e", alturaPadrao, larguraPadrao);
+const celula22 = new Tabuleiro(cor2, "150", "60", "6", "f", alturaPadrao, larguraPadrao);
+const celula23 = new Tabuleiro(cor1, "180", "60", "6", "g", alturaPadrao, larguraPadrao);
+const celula24 = new Tabuleiro(cor2, "210", "60", "6", "h", alturaPadrao, larguraPadrao);
+
+
+
+
+const celula25 = new Tabuleiro(cor2, "0", "90", "5", "a", alturaPadrao, larguraPadrao);
+const celula26 = new Tabuleiro(cor1, "30", "90", "5", "b", alturaPadrao, larguraPadrao);
+const celula27 = new Tabuleiro(cor2, "60", "90", "5", "c", alturaPadrao, larguraPadrao);
+const celula28 = new Tabuleiro(cor1, "90", "90", "5", "d", alturaPadrao, larguraPadrao);
+const celula29 = new Tabuleiro(cor2, "120", "90", "5", "e", alturaPadrao, larguraPadrao);
+const celula30 = new Tabuleiro(cor1, "150", "90", "5", "f", alturaPadrao, larguraPadrao);
+const celula31 = new Tabuleiro(cor2, "180", "90", "5", "g", alturaPadrao, larguraPadrao);
+const celula32 = new Tabuleiro(cor1, "210", "90", "5", "h", alturaPadrao, larguraPadrao);
+
+
+
+
+const celula33 = new Tabuleiro(cor1, "0", "120", "4", "a", alturaPadrao, larguraPadrao);
+const celula34 = new Tabuleiro(cor2, "30", "120", "4", "b", alturaPadrao, larguraPadrao);
+const celula35 = new Tabuleiro(cor1, "60", "120", "4", "c", alturaPadrao, larguraPadrao);
+const celula36 = new Tabuleiro(cor2, "90", "120", "4", "d", alturaPadrao, larguraPadrao);
+const celula37 = new Tabuleiro(cor1, "120", "120", "4", "e", alturaPadrao, larguraPadrao);
+const celula38 = new Tabuleiro(cor2, "150", "120", "4", "f", alturaPadrao, larguraPadrao);
+const celula39 = new Tabuleiro(cor1, "180", "120", "4", "g", alturaPadrao, larguraPadrao);
+const celula40 = new Tabuleiro(cor2, "210", "120", "4", "h", alturaPadrao, larguraPadrao);
+
+
+
+
+const celula41 = new Tabuleiro(cor2, "0", "150", "3", "a", alturaPadrao, larguraPadrao);
+const celula42 = new Tabuleiro(cor1, "30", "150", "3", "b", alturaPadrao, larguraPadrao);
+const celula43 = new Tabuleiro(cor2, "60", "150", "7", "c", alturaPadrao, larguraPadrao);
+const celula44 = new Tabuleiro(cor1, "90", "150", "3", "d", alturaPadrao, larguraPadrao);
+const celula45 = new Tabuleiro(cor2, "120", "150", "3", "e", alturaPadrao, larguraPadrao);
+const celula46 = new Tabuleiro(cor1, "150", "150", "3", "f", alturaPadrao, larguraPadrao);
+const celula47 = new Tabuleiro(cor2, "180", "150", "3", "g", alturaPadrao, larguraPadrao);
+const celula48 = new Tabuleiro(cor1, "210", "150", "3", "h", alturaPadrao, larguraPadrao);
+
+
+
+
+const celula49 = new Tabuleiro(cor1, "0", "180", "2", "a", alturaPadrao, larguraPadrao);
+const celula50 = new Tabuleiro(cor2, "30", "180", "2", "b", alturaPadrao, larguraPadrao);
+const celula51 = new Tabuleiro(cor1, "60", "180", "2", "c", alturaPadrao, larguraPadrao);
+const celula52 = new Tabuleiro(cor2, "90", "180", "2", "d", alturaPadrao, larguraPadrao);
+const celula53 = new Tabuleiro(cor1, "120", "180", "2", "e", alturaPadrao, larguraPadrao);
+const celula54 = new Tabuleiro(cor2, "150", "180", "2", "f", alturaPadrao, larguraPadrao);
+const celula55 = new Tabuleiro(cor1, "180", "180", "2", "g", alturaPadrao, larguraPadrao);
+const celula56 = new Tabuleiro(cor2, "210", "180", "2", "h", alturaPadrao, larguraPadrao);
+
+
+
+const celula57 = new Tabuleiro(cor2, "0", "210", "1", "a", alturaPadrao, larguraPadrao);
+const celula58 = new Tabuleiro(cor1, "30", "210", "1", "b", alturaPadrao, larguraPadrao);
+const celula59 = new Tabuleiro(cor2, "60", "210", "1", "c", alturaPadrao, larguraPadrao);
+const celula60 = new Tabuleiro(cor1, "90", "210", "1", "d", alturaPadrao, larguraPadrao);
+const celula61 = new Tabuleiro(cor2, "120", "210", "1", "e", alturaPadrao, larguraPadrao);
+const celula62 = new Tabuleiro(cor1, "150", "210", "1", "f", alturaPadrao, larguraPadrao);
+const celula63 = new Tabuleiro(cor2, "180", "210", "1", "g", alturaPadrao, larguraPadrao);
+const celula64 = new Tabuleiro(cor1, "210", "210", "1", "h", alturaPadrao, larguraPadrao);
+
+
+
+
